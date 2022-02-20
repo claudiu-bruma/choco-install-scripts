9728e64002cb62db03454f32154e0f62be9c4f1c
Claudiu Bruma <claudiu.bruma@gmail.com>
2022-02-20 05:43:40 -0800

    added powershell script
    added powershell script with choco install for some apps

---
+++ choco install stuff.ps1
@@ -0,0 +1,53 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+
+
+choco install adobereader -y
+choco install 7zip -y
+choco install powertoys -y
+choco install totalcommander -y
+
+choco install f.lux -y
+
+choco install googlechrome -y 
+choco install ublockorigin-chrome -y
+choco install adblockpluschrome -y
+
+
+choco install firefox -y
+choco install adblockplus-firefox -y
+choco install firefoxpwa -y
+
+choco install opera -y
+
+choco install sublimetext4 -y
+choco install notepadplusplus -y
+choco install npppluginmanager -y
+choco install typora -y
+
+choco install git -y
+
+choco install git-fork -y
+choco install gitkraken -y
+choco install sourcetree -y 
+choco install github-desktop  -y
+ 
+choco install dotnet-6.0-sdk -y
+choco install dotnet-5.0-sdk -y
+choco install dotnetcore-3.1-sdk -y
+choco install netfx-4.8-devpack -y
+
+choco install visualstudio2019community -y
+choco install visualstudio2022community -y
+choco install visualstudio-github -y
+
+choco install vscode -y
+choco install vscode-code-runner -y
+choco install vscode-prettier -y
+choco install vscode-powershell -y
+
+choco install docker-desktop -y
+
+
+
+ 
